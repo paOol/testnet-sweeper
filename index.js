04f2e67bacec88861142557e0a83a47063e59707
paool <paoolkim@gmail.com>
2020-01-14 19:59:57 -0800

    add scheduler

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const BCHnode = require('bitcoin-cash-rpc');
 
-const { host, username, password, port } = require('./config/config.js');
+const {
+  host,
+  username,
+  password,
+  port,
+  sendTo
+} = require('./config/config.js');
 
 const bch = new BCHnode(host, username, password, port, 3000, false);
 
-(async function () {
-  let asdf = await bch.getInfo('');
+const schedule = require('node-schedule');
+
+async function getBalance() {
+  const { balance } = await bch.getWalletInfo('');
+  return balance;
+}
+
+async function send(amount) {
+  const result = await bch.sendToAddress(sendTo, amount);
+  return result;
+}
+
+async function run() {
+  const balance = await getBalance();
+
+  if (balance <= 0.000006) {
+    return;
+  }
+
+  const txid = await send(balance);
+  console.log(`sent ${balance}. TXID: ${txid}`);
+  return;
+}
+
+(async function() {
+  schedule.scheduleJob('*/51 * * * *', async () => {
+    run();
+    console.log('ran update on  ', Date.now());
+  });
 })();
